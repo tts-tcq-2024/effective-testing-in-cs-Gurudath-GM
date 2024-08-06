aeac0d6e1853a55f23f62db5079c6feb7bdfa4a0
Gurudath G M <princegguruk@gmail.com>
2024-08-06 22:22:22 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,23 +1,26 @@
-using System;
-using System.Diagnostics;
+using MisalignedSpaceTests;
 
-namespace MisalignedSpace {
-    class Misaligned {
-        static int printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+namespace MisalignedSpace
+{
+    class Misaligned
+    {
+        public static int PrintColorMap()
+        {
+            string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+            string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
             int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
                     Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
                 }
             }
             return i * j;
         }
-        static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
-            Console.WriteLine("All is well (maybe!)");
+        static void Main(string[] args)
+        {
+            MisalignedTests.TestPrintColorMapAndReturnsPairsCount();
         }
     }
 }
