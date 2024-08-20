f511b3d0ed2e9cfc94f23f03303eb1eb0367bb72
Gurudath G M <princegguruk@gmail.com>
2024-08-20 14:39:29 +0530

    Update TestTshirtSize.cs

--- tshirts/TestTshirtSize.cs
+++ tshirts/TestTshirtSize.cs
@@ -4,41 +4,28 @@ using System.Diagnostics;
 namespace TshirtSpace {
     class TestTshirtSize
     {
-        private static bool isPassed = false;
         public static void Size_ShouldReturn_Small_ForMeasurementsLessThanOrEqualTo38(int cms, string expected)
         {
             string actual = Tshirt.Size(cms);
-            isPassed = Debug.Equals(expected, actual);
-            CheckTest_status(isPassed);
+            Debug.Equals(actual, expected);
         }
 
         public static void Size_ShouldReturn_Medium_ForMeasurementsBetween38And42(int cms, string expected)
         {
             string actual = Tshirt.Size(cms);
-            isPassed = Debug.Equals(expected, actual);
-            CheckTest_status(isPassed);
+            Debug.Equals(actual, expected);
         }
 
         public static void Size_ShouldReturn_Large_ForMeasurements42AndAbove(int cms, string expected)
         {
             string actual = Tshirt.Size(cms);
-            isPassed = Debug.Equals(expected, actual);
-            CheckTest_status(isPassed);
+            Debug.Equals(actual, expected);
         }
 
         public static void Size_ShouldReturn_Exception_ForNegativeMeasurements(int cms, string expected)
         {
             string actual = Tshirt.Size(cms);
-            isPassed = Debug.Equals(actual, expected);
-            CheckTest_status(isPassed);
-        }
-
-        private static void CheckTest_status(bool isPassed)
-        {
-            if (isPassed)
-                Console.WriteLine("Test passed");
-            else
-                throw new Exception("Test failed");
+            Debug.Equals(actual, expected);
         }
     }
 }
