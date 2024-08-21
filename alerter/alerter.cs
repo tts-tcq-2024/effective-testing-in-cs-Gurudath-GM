5dbf0332b5d8ac4014881d572e566110490e463e
Gurudath G M <princegguruk@gmail.com>
2024-08-21 22:51:38 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,31 +1,32 @@
-using System;
 using System.Diagnostics;
 
-static class Program
+namespace AlerterSpace
 {
-    private static int alertFailureCount = 0;
-
-    private static void AlertInCelsius(INetworkAlert alertSystem, float fahrenheit)
+    class Alerter
     {
-        float celsius = (fahrenheit - 32) * 5 / 9;
-        int returnCode = alertSystem.SendAlert(celsius);
-        if (returnCode != 200)
-        {
- 
-            alertFailureCount += 1;
-        }
-    }
+            private static int alertFailureCount = 0;
 
-    public static void Main()
-    {
-        INetworkAlert stubAlert = new NetworkAlertStub();
+            private static void AlertInCelsius(INetworkAlert alertSystem, float fahrenheit)
+            {
+                float celsius = (fahrenheit - 32) * 5 / 9;
+                int returnCode = alertSystem.SendAlert(celsius);
+                if (returnCode != 200)
+                {
+                    alertFailureCount += 1;
+                }
+            }
 
-        alertFailureCount = 0;
-        AlertInCelsius(stubAlert, 400.5f); 
-        Debug.Assert(alertFailureCount == 1, "Test failed, expected failure count = 1");
-        AlertInCelsius(stubAlert, 303.6f); 
-        Debug.Assert(alertFailureCount == 2, "Test failed, expected failure count = 2");
-        Console.WriteLine($"{alertFailureCount} alerts failed.");
-        Console.WriteLine("All is well (maybe!)");
+            public static void Main()
+            {
+                INetworkAlert stubAlert = new NetworkAlertStub();
+
+                alertFailureCount = 0;
+                AlertInCelsius(stubAlert, 400.5f);
+                Debug.Assert(alertFailureCount == 1, "Test failed, expected failure count = 1");
+                AlertInCelsius(stubAlert, 303.6f);
+                Debug.Assert(alertFailureCount == 2, "Test failed, expected failure count = 2");
+                Console.WriteLine($"{alertFailureCount} alerts failed.");
+                Console.WriteLine("All is well (maybe!)");
+            }
     }
 }
